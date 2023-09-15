05b9d0363123106e252171543db7c9550f049c79
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2023-09-15 12:46:41 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react'
+import CartContainer from './components/CartContainer'
+import Navbar from './components/Navbar'
+import { useSelector, useDispatch } from 'react-redux'
+import { calculateTotals } from './features/cart/cartSlice'
+import Modal from './components/Modal'
+
+const App = () => {
+  const { cartItems } = useSelector(state => state.cart)
+  const { isOpen } = useSelector((state) => state.modal)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(calculateTotals())
+  }, [cartItems])
+  return <main>
+    {isOpen && <Modal />}
+    <Navbar />
+    <CartContainer />
+  </main>
+}
+export default App
